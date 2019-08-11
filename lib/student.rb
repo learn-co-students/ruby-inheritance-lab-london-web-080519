c458417cd4d1d4fbf0d2c62c721a6e78fb7571dd
oliburt <oliburt@gmail.com>
2019-08-11 18:31:37 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+
+
+    def initialize
+        @knowledge = []
+        @@all << self
+    end
+
+    def learn(knowledge_string)
+        @knowledge << knowledge_string
+    end
+
+    def self.all
+        @@all.select{|user| user.class == Student }
+      end
 
 end
