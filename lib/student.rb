d59011ff527146119ae7794384c02ae1d57ba8fe
github username <aude53@gmail.com>
2019-08-13 15:20:28 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,15 @@
 class Student < User
 
-    attr_reader :knowledge
-
     def initialize
         @knowledge = []
     end 
 
     def learn(new_skill)
         @knowledge << new_skill
+    end
+
+    def knowledge
+        @knowledge
     end
 
 end
