79ba0382b65481eb293863f8389914c780e8236b
github username <aude53@gmail.com>
2019-08-13 15:20:06 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn(new_skill)
+        @knowledge << new_skill
+    end
 
 end
